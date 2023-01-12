b78abd610c00d2f86a020190522f68ce5d9c57e5
oleksandrmaliy <115729512+oleksandrmaliy@users.noreply.github.com>
2023-01-12 21:52:26 +0200

    initial layout
    added first layout

---
+++ src/js/gallery.js
@@ -0,0 +1,9 @@
+$('.gallery-js').slick({
+  dots: false,
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 2000,
+});
---
+++ src/js/reviews.js
@@ -0,0 +1,8 @@
+$('.reviews-js').slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
