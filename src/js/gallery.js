e68d8751dded1afcdf27526472a66029f40f3c8f
oleksandrmaliy <115729512+oleksandrmaliy@users.noreply.github.com>
2023-01-19 21:25:26 +0200

    Gallery completed

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,6 +3,7 @@ $('.gallery__slider').slick({
   arrows: false,
   slidesToShow: 1,
   slidesToScroll: 1,
+  infinite: true,
   autoplay: true,
   autoplaySpeed: 2000,
   speed: 2000,
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,8 +1,10 @@
 $('.reviews__slider').slick({
   dots: true,
   arrows: false,
-  infinite: true,
-  speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
+  infinite: true,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  speed: 2000,
 });
